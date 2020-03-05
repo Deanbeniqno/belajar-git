4963599f6d683d11dcc9e641bb66a199f229cc1e
Dean Beniqno A <dbpower20@gmail.com>
2020-03-05 12:04:30 +0700

    Revisi pertama 2 file baru

--- hapus.php
+++ hapus.php
@@ -1,3 +1,4 @@
 <?php
     echo "Ini adalah file baru, yaitu hapus.php ";
+    echo "Ada penambahan revisi pada file ini";
 ?>
--- tambah.php
+++ tambah.php
@@ -1,3 +1,4 @@
 <?php
     echo "Ini adalah file baru, yaitu tambah.php ";
+    echo "Ada penambahan revisi pada file ini";
 ?>
